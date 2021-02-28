f28fbf7ab259507e557219114ac600005c577227
Ak-Shaw <ayushshawz@gmail.com>
2021-02-28 19:04:48 +0530

    added 2nd script for day 3

---
+++ Day3/script_3_2_sum_with_input.py
@@ -0,0 +1,10 @@
+# accept a value for `a` as an integer from the user
+a = int(input("a = "))
+
+# accept a value for `b` as an integer from the user
+b = int(input("b = "))
+
+sum = a + b
+
+# print the sum to the console
+print("Sum of a and b is", sum)
