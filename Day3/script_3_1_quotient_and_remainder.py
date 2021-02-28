c68ba5510746480cc03ff071ca64d57e15ed9982
Ak-Shaw <ayushshawz@gmail.com>
2021-02-28 18:54:55 +0530

    added first script of day 3

---
+++ Day3/script_3_1_quotient_and_remainder.py
@@ -0,0 +1,17 @@
+# Find the quotient and remainder on dividing 37 with 4.
+
+# Sample Ouptut:
+'''
+On the dividing 37 with 4 the result is as follows:
+Quotient = 9
+Remainder = 1
+'''
+
+dividend = 37
+divisor = 4
+quotient = dividend // divisor
+remainder = dividend - quotient * divisor
+
+print ("On the dividing 37 with 4 the result is as follows:")
+print ("Quotient =", quotient )
+print ("Remainder =", remainder )
