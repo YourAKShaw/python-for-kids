71867ae5906ac2fa6c5893ac53ac0563c9ee7aee
Ak-Shaw <ayushshawz@gmail.com>
2021-08-28 13:10:37 +0530

    Added basicTerminalCommands.sh as part of Day4.

---
+++ Day4/basicTerminalCommands.sh
@@ -0,0 +1,17 @@
+# Open a file in Sublime Text.
+subl fileName.py
+
+# List all files and folders in the current directory.
+ls
+
+# Clear the terminal screen.
+clear
+
+# Execute a Python script.
+python fileName.py
+
+# Print history of commands.
+history
+
+# Delete file.
+rm fileName.py
