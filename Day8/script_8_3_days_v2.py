19a246e2dbe2c8e91cb594e3a63bca22504db914
Ak-Shaw <ayushshawz@gmail.com>
2021-10-30 12:36:05 +0530

    Added Day8 assignment.

---
+++ Day8/script_8_3_days_v2.py
@@ -0,0 +1,8 @@
+'''
+Accept a day number from the user, and display the day of the week that day is.
+
+    1. If the user enters a number between 1 and 7, then display the day of the week that number represents.
+    2. If the user enters a number that is less than 1 or greater than 7, then display an error message.
+
+NOTE: Make use of an if-elif-else ladder.
+'''
