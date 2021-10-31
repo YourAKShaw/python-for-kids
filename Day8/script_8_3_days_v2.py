a0590855160191ca06790a68b9767698b86967eb
Ak-Shaw <ayushshawz@gmail.com>
2021-10-31 17:20:33 +0530

    Added solution for Day 8 assignment.

--- Day8/script_8_3_days_v2.py
+++ Day8/script_8_3_days_v2.py
@@ -6,3 +6,22 @@ Accept a day number from the user, and display the day of the week that day is.
 
 NOTE: Make use of an if-elif-else ladder.
 '''
+
+day_number = int(input("Enter the day number: "))
+
+if day_number == 1:
+    print("Monday")
+elif day_number == 2:
+    print("Tuesday")
+elif day_number == 3:
+    print("Wednesday")
+elif day_number == 4:
+    print("Thursday")
+elif day_number == 5:
+    print("Friday")
+elif day_number == 6:
+    print("Saturday")
+elif day_number == 7:
+    print("Sunday")
+else:
+    print("Invalid option!")
