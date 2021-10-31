a681d4ab627f2c7f8ce9662a5b612ec067f12960
Ak-Shaw <ayushshawz@gmail.com>
2021-10-31 17:48:27 +0530

    Added assignment for day 9 with incomplete code.

---
+++ Day9/script_9_3_electricity_bill.py
@@ -0,0 +1,17 @@
+'''
+Given an integer U denoting the amount of KWh (Kilo-Watt-hour) units of electricity consumed, the task is to calculate the electricity bill with the help of the below charges: 
+
+1 to 100 units – Rs. 10/unit
+100 to 200 units – Rs. 15/unit
+200 to 300 units – Rs. 20/unit
+above 300 units – Rs. 25/unit
+'''
+
+units_consumed = int(input("Enter the units consumed: "))
+
+if (units_consumed >= 1 and units_consumed <= 100):
+    bill = units_consumed * 10
+elif (units_consumed >= 101 and units_consumed <= 200):
+    bill = 100 * 10 + (units_consumed - 100) * 15
+
+print("The electricity bill is: ", bill)
