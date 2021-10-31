fa48b77167523d86e962f0b0945c16d3fa589c07
Ak-Shaw <ayushshawz@gmail.com>
2021-10-31 17:30:35 +0530

    Added first script for day 9.

---
+++ Day9/script_9_1_logical_operators.py
@@ -0,0 +1,22 @@
+# Logical Operators in Python
+
+'''
+The logical operators in Python are used to combine conditional statements.
+
+These are:
+
+- and
+- or
+- not
+
+- and: Returns True if both statements are True
+- or: Returns True if one of the statements is True
+- not: Returns True if the statement is False
+'''
+
+print('5 < 6 and 4 < 6 is', 5 < 6 and 4 < 6)
+print('5 < 6 and 6 < 4 is', 5 < 6 and 6 < 4)
+print('5 < 6 or 6 < 4 is', 5 < 6 or 6 < 4)
+print('5 > 6 or 6 < 4 is', 5 > 6 or 6 < 4)
+print('not 5 < 6 is', not 5 < 6)
+print('not 5 > 6 is', not 5 > 6)
