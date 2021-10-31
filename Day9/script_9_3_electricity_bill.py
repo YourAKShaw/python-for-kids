1bf418a554cdfac9ed4b12d4e6fc607ebc4fc980
Ak-Shaw <ayushshawz@gmail.com>
2021-10-31 20:43:36 +0530

    Added solution for Day 9 assignment.

--- Day9/script_9_3_electricity_bill.py
+++ Day9/script_9_3_electricity_bill.py
@@ -13,5 +13,9 @@ if (units_consumed >= 1 and units_consumed <= 100):
     bill = units_consumed * 10
 elif (units_consumed >= 101 and units_consumed <= 200):
     bill = 100 * 10 + (units_consumed - 100) * 15
+elif (units_consumed >= 201 and units_consumed <= 300):
+    bill = 100 * 10 + 100 * 15 + (units_consumed - 200) * 20
+elif (units_consumed >= 301):
+    bill = 100 * 10 + 100 * 15 + 100 * 20 + (units_consumed - 300) * 20
 
 print("The electricity bill is: ", bill)
