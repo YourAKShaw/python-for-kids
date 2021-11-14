429ecfde679daf8dbef10c3e2afd3e6891fcc0fb
Ak-Shaw <ayushshawz@gmail.com>
2021-11-14 18:38:02 +0530

    Added solution of Day 12 HW.

--- Day12/script_12_6_natural_sum.py
+++ Day12/script_12_6_natural_sum.py
@@ -6,3 +6,9 @@ If n = 10, then sum = 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10 = 55
 
 [NOTE: Use for loop to solve this problem.]
 '''
+
+n = int(input("Enter the number: "))
+total = 0
+for i in range(1, n + 1):
+    total = total + i
+print("Sum of first", n, "natural numbers is:", total)
