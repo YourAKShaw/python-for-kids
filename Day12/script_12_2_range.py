216b5cf9a263fa95e0f7a163b7094f4837a1bc83
Ak-Shaw <ayushshawz@gmail.com>
2021-11-20 12:17:09 +0530

    Added pattern 1 as a script for day 13 and updated day 12 code.

--- Day12/script_12_2_range.py
+++ Day12/script_12_2_range.py
@@ -14,6 +14,8 @@ range(x, y, z)
 x - is the starting value; default value is 0 (zero)
 y - is the upper bound or lower bound, greater than the highest value in the range or lesser than the least value in the range; this value should always be given
 z - is the step value, default value is 1 
+
+Other forms of using range is range(x, y) and range(y)
 '''
 
 l = list(range(0, 10, 1))
@@ -25,3 +27,6 @@ print('list(range(0, 10, 2))', l)
 
 l = list(range(10, 0, -1))
 print('list(range(10, 0, -1))', l)
+
+l = list(range(1, 6))
+print('list(range(1, 6))', l)
---
+++ Day13/script_13_4_pattern_1.py
@@ -0,0 +1,32 @@
+# For n = 5, print the following pattern:
+'''
+1
+1   2
+1   2   3
+1   2   3   4
+1   2   3   4   5
+'''
+# The pattern should grow for various values of n, and should be symmetrical.
+# n should be a positive value.
+
+
+def print_pattern(n):
+    for i in range(1, n + 1):
+        for j in range(1, i + 1):
+            print(j, end="\t")
+        print()
+
+
+# driver code
+if __name__ == "__main__":
+    n = int(input("Enter the value of n: "))
+    if (n < 1):
+        print("n should be a positive value")
+    else:
+        print_pattern(n)
+
+
+# -------------------------------------
+'''
+
+'''
