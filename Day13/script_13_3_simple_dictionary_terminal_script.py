8b3835c8893e839881eb0214c65c4ddab7038b45
Ak-Shaw <ayushshawz@gmail.com>
2021-11-20 11:57:53 +0530

    Added solution for Day13 classwork.

--- Day13/script_13_3_simple_dictionary_terminal_script.py
+++ Day13/script_13_3_simple_dictionary_terminal_script.py
@@ -8,3 +8,20 @@ words = {
 '''
 # Accept a word from user and print its definition if found, else print "Word is absent in our dictionary".
 
+
+def get_meaning_of_word(word):
+
+    # dictionary of words with their meanings
+    words = {
+        "Herbivores": 'Animals that only eat plants.',
+        "Carnivores": 'Animals that eat only flesh of other other animals.',
+        "Omnivores":  'Animals that eat both flesh and plants.',
+        "Scavengers": 'Animals that eat dead or decaying animals.',
+        "Decomposers": 'Organisms that eat dead or deecaying plants and animals.'
+    }
+
+    return words.get(word, "Word is absent in our dictionary.")
+
+
+word = input('Enter a word: ')
+print(get_meaning_of_word(word))
