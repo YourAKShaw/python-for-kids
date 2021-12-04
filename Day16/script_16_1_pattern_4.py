501d002495d61601d5335c9fca4a51dccec0fc70
Ak-Shaw <ayushshawz@gmail.com>
2021-12-04 11:25:00 +0530

    Added first script for day 16.

---
+++ Day16/script_16_1_pattern_4.py
@@ -0,0 +1,26 @@
+# For n = 5, print the following pattern:
+'''
+1
+1 0
+1 0 1
+1 0 1 0
+1 0 1 0 1
+'''
+# The pattern should grow for various values of n, and should be symmetrical.
+# n should be a positive value.
+
+
+def print_pattern(n):
+    for i in range(1, n + 1):
+        for j in range(1, i + 1):
+            print(j % 2, end=' ')
+        print()
+
+
+# driver code
+if __name__ == "__main__":
+    n = int(input("Enter the value of n: "))
+    if (n < 1):
+        print("n should be a positive value")
+    else:
+        print_pattern(n)
