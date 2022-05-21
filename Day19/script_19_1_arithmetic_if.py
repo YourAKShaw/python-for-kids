d97e3ea7b1d332af55828c81c035f7a5c671b31e
Ak-Shaw <ayushshawz@gmail.com>
2022-05-21 11:21:05 +0530

    add CW test question for Day 19

---
+++ Day19/script_19_1_arithmetic_if.py
@@ -0,0 +1,10 @@
+'''
+Accept two numbers from user and a character.
+
+If the character is '+' then add the two numbers and print the result.
+If the character is '-' then subtract the two numbers and print the result.
+If the character is '*' then multiply the two numbers and print the result.
+If the character is '/' then divide the two numbers and print the result.
+
+In case of any other character print "Invalid input"
+'''
