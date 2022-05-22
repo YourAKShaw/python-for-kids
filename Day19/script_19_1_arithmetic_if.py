b491dfbdd17b9e5b1e23d6b5f9c4fe877c6100a5
Ak-Shaw <ayushshawz@gmail.com>
2022-05-22 18:19:16 +0530

    content(main): add solution for day 19 CW

--- Day19/script_19_1_arithmetic_if.py
+++ Day19/script_19_1_arithmetic_if.py
@@ -8,3 +8,19 @@ If the character is '/' then divide the two numbers and print the result.
 
 In case of any other character print "Invalid input"
 '''
+
+a = int(input("Enter the value of a: "))
+b = int(input("Enter the value of b: "))
+c = input("Enter the action character: ")
+print()
+
+if c == '+':
+    print("The sum result of the two numbers is: ", a + b)
+elif c == '-':
+    print("The difference result of the two numbers is: ", a - b)
+elif c == '*':
+    print("The product result of the two numbers is: ", a * b)
+elif c == '/':
+    print("The division result of the two numbers is: ", a / b)
+else:
+    print("Invalid Input!")
